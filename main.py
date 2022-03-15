63b268abb8f51648ea280f2fa741f5a27708f28f
Cornelius Jermaine Hightower <75323116+Cjhightower38@users.noreply.github.com>
2022-03-15 13:53:58 +0000

    complete step 2 by using the range and len features

--- main.py
+++ main.py
@@ -8,19 +8,24 @@ random_word = random.choice(word_list)
 print(f'Pssst, the solution is {random_word}.')
 
 #TODO-1: - Create an empty List called display.
+display = []
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+for words in random_word:
+  display.append('_')
+# print(display)
 
 guess = input('Please guess a letter\n').lower()
 
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for words in random_word:
+for position in range(len(random_word)):
+  words = random_word[position]
   if guess in words:
-    print('Right')
-  else:
-    print('Wrong')
+    display[position] = guess
+
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+print(display)
